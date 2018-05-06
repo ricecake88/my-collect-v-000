132fdeba3639e2f3bf0ce394f865f18c0fc402df
Grace Shih <vocalise@gmail.com>
2018-05-06 18:07:14 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
 
+def my_collect(array)
+  i = 0
+  collections = []
+  while i < array.length
+     collections << yield(array[i])
+     i += 1
+  end
+  return collections
+end
 
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+newArray = my_collect(array) do |name|
+  name.split(" ").first
+end
+
+puts(newArray)
